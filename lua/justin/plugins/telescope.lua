4cae7c60a55ded0762560cb53699db49eed638ac
Justin Lafferty <justin.lafferty@kw.com>
2023-09-23 15:18:51 -0400

    telescope extensions

--- lua/justin/plugins/telescope.lua
+++ lua/justin/plugins/telescope.lua
@@ -1,10 +1,17 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.3',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-dap.nvim' },
     config = function()
+        local telescope = require('telescope')
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+        telescope.load_extension("flutter")
+        vim.keymap.set('n', '<leader>pm', telescope.extensions.flutter.commands)
+
+        telescope.load_extension("dap")
+        vim.keymap.set('n', '<leader>pd', telescope.extensions.dap.commands)
     end
 }
