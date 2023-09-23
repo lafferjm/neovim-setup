7c807195342291e5aa947638be46bd14c3d8143e
Justin Lafferty <justin.lafferty@kw.com>
2023-09-23 10:16:14 -0400

    fix flutter-tools/treesitter conflict

--- lua/justin/plugins/treesitter.lua
+++ lua/justin/plugins/treesitter.lua
@@ -1,20 +1,25 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function ()
-		local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"dart",
-				"javascript",
-				"typescript",
-				"lua",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end
+        configs.setup({
+            ensure_installed = {
+                "dart",
+                "javascript",
+                "typescript",
+                "lua",
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = { enable = true },
+            indent = {
+                enable = true,
+                disable = {
+                    'dart'
+                }
+            },
+        })
+    end
 }
