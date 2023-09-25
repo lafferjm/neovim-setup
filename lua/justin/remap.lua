c84fed4a0ad746bdc83044b97d18f8ceee684551
Justin Lafferty <justin.lafferty@kw.com>
2023-09-25 16:08:56 -0400

    add window movement bindings

--- lua/justin/remap.lua
+++ lua/justin/remap.lua
@@ -18,7 +18,16 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
-vim.keymap.set("n", "<leader>wj", "<C-w>j")
-vim.keymap.set("n", "<leader>wk", "<C-w>k")
-vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>wH", "<C-w>H")
 
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wJ", "<C-w>J")
+
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
+vim.keymap.set("n", "<leader>wK", "<C-w>K")
+
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>wL", "<C-w>L")
+
+vim.keymap.set("n", "<leader>wr", "<C-w>r")
+vim.keymap.set("n", "<leader>wR", "<C-w>R")
