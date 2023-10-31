ac969c73275b7fb75cd102575bcbae138137bbe6
Justin Lafferty <justin.lafferty@kw.com>
2023-10-31 12:10:35 -0400

    fix yank mapping

--- lua/justin/remap.lua
+++ lua/justin/remap.lua
@@ -6,9 +6,8 @@ vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
