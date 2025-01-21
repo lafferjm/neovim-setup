e3e9509a425d1be85018478aef90535ccde20540
Justin Lafferty <justin.lafferty@kw.com>
2025-01-21 08:40:17 -0500

    add typescript and javascript formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,8 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'eslint_d' },
     },
     format_on_save = {
       timeout_ms = 500,
