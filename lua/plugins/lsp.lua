4ac9426a6d1ef78a6516ad1d38abe9e304a1895e
Justin Lafferty <justin.lafferty@kw.com>
2025-01-22 09:06:04 -0500

    enable blink experimental signature

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,6 +19,7 @@ return {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
       },
+      signature = { enabled = true },
       sources = {
         cmdline = {},
         default = { 'lsp', 'path', 'buffer' },
