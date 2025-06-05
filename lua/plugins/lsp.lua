1f082aa8099666566f2aaa9d07369b0b0a7c184e
Justin Lafferty <justin.lafferty@kw.com>
2025-06-05 08:42:43 -0400

    simplify lsp setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,10 @@
 return {
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
   {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
+    'mason-org/mason-lspconfig.nvim',
+    opts = {},
+    dependencies = {
+      { 'mason-org/mason.nvim', opts = {} },
+      'neovim/nvim-lspconfig',
     },
   },
   {
@@ -25,34 +22,5 @@ return {
       },
     },
     opts_extend = { 'sources.default' },
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = 'lazydev',
-        group_index = 0,
-      })
-    end,
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { 'j-hui/fidget.nvim', opts = {} },
-    },
-    config = function()
-      require('mason').setup()
-      require('mason-lspconfig').setup {}
-
-      local map = function(keys, func, desc, mode)
-        mode = mode or 'n'
-        vim.keymap.set(mode, keys, func, { desc = 'LSP: ' .. desc })
-      end
-
-      map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-      map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-      map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    end,
   },
 }
